f6e2ee371d4f39d585c2b3760851108ae3175a36
mr-zero-03 <danielsantiagoperez03@gmail.com>
2021-10-05 14:16:28 -0500

    Added student_table, the first table

---
+++ student_table.sql
@@ -0,0 +1,13 @@
+-- To create the student table, and alterate it to add a new field
+
+CREATE TABLE student (
+  id INT,
+  name VARCHAR(30),
+  grade TINYINT(2) UNSIGNED,
+
+  PRIMARY KEY(id)
+);
+DESCRIBE student;
+
+ALTER TABLE student ADD birthday_date DATE;
+DESCRIBE student;
